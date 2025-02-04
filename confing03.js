4bbc581469bbc6320176860172ba8dd68c3f9629
Daniil Shulga <158099363+Polkan16@users.noreply.github.com>
2025-02-04 19:59:10 +0000

    Update confing03.js

--- confing03.js
+++ confing03.js
@@ -1,4 +1,7 @@
-// config.js
 const CONFIG = {
-  DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/your_webhook_url_here'
-};
+  DISCORD_WEBHOOK_URL:
+    "https://discord.com/api/webhooks/1336422613685108876/Vt4uzeEHTyHk_Ib57VPkkGq6raWNcMALoRXd0Lm5Pu7S0Ry-NYkrL6vqkwVPrBEaP_4u",
+  GOOGLE_SHEET_URL:
+    "https://script.google.com/macros/s/AKfycbzdMWQeXvdHKaCzLvCR80ji8bkYdyEYQ7G4I10w2I7zInnZQ2saFTVPGSXEBMwP61ht/exec",
+}
+
