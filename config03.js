13c3e8c8a97e6f45594bdd89d17cf9303d009bce
Daniil Shulga <158099363+Polkan16@users.noreply.github.com>
2025-02-04 21:12:30 +0000

    Delete config03.js

--- config03.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  DISCORD_WEBHOOK_URL:
-    "https://discord.com/api/webhooks/1336422613685108876/Vt4uzeEHTyHk_Ib57VPkkGq6raWNcMALoRXd0Lm5Pu7S0Ry-NYkrL6vqkwVPrBEaP_4u",
-  GOOGLE_SHEET_URL:
-    "https://script.google.com/macros/s/AKfycbzdMWQeXvdHKaCzLvCR80ji8bkYdyEYQ7G4I10w2I7zInnZQ2saFTVPGSXEBMwP61ht/exec",
-}
-
